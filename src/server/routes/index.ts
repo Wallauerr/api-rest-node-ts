d7971df8547cf76db16802c7c05ae2f4e20a21a2
Wallauerr <viniwallauer@hotmail.com>
2023-11-07 00:04:55 -0300

    add folders structure

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -11,7 +11,7 @@ router.get('/', (_, res) => {
 
 
 router.post('/teste', (req, res) => {
-  console.log(req.body);
+  // console.log(req.body);
   
   return res.status(StatusCodes.UNAUTHORIZED).json(req.body);
 });
